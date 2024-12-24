8b4ddbd5d9d74d0d31501bbaecde6dc1bb6e0aaf
RockyKiran <rockykiran8085@gmail.com>
2024-12-24 15:08:38 +0530

    Implement Hibernate and create table in database

---
+++ HIBERNATE-1/src/main/java/TestMain.java
@@ -0,0 +1,11 @@
+import javax.persistence.Persistence;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Persistence.createEntityManagerFactory("rocky");
+
+	}
+
+}
